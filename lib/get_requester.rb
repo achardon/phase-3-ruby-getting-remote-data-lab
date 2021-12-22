073391dea2fdbf378764ec4c5ba6ab06eb985a49
Alessandra Chardon <achardon@ucla.edu>
2021-12-22 15:42:30 -0800

    commit tree 424217fc2c2de4efbae13b97d0ac84d71952cd7c

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,26 @@
-# Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+class GetRequester
+
+    attr_accessor :url
+
+    def initialize(url)
+        @url = url
+    end
+
+    def get_response_body
+        #sends GET request to URL
+        uri = URI.parse(self.url)
+        response = Net::HTTP.get_response(uri)
+        #returns body of response
+        response.body
+    end
+
+    def parse_json
+        JSON.parse(self.get_response_body)
+
+    end
+
+end
